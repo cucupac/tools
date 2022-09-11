09e50b69c9020019b4b7debc8b44ffc68cb21c09
Adam Cuculich <cucupac1996@gmail.com>
2022-09-11 15:21:43 -0500

    Push after testing loan on user's behalf.

---
+++ scripts/aave_oracle_abi.py
@@ -0,0 +1,229 @@
+ABI = [
+    {
+        "inputs": [
+            {
+                "internalType": "contract IPoolAddressesProvider",
+                "name": "provider",
+                "type": "address"
+            },
+            {
+                "internalType": "address[]",
+                "name": "assets",
+                "type": "address[]"
+            },
+            {
+                "internalType": "address[]",
+                "name": "sources",
+                "type": "address[]"
+            },
+            {
+                "internalType": "address",
+                "name": "fallbackOracle",
+                "type": "address"
+            },
+            {
+                "internalType": "address",
+                "name": "baseCurrency",
+                "type": "address"
+            },
+            {
+                "internalType": "uint256",
+                "name": "baseCurrencyUnit",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "constructor"
+    },
+    {
+        "anonymous": False,
+        "inputs": [
+            {
+                "indexed": True,
+                "internalType": "address",
+                "name": "asset",
+                "type": "address"
+            },
+            {
+                "indexed": True,
+                "internalType": "address",
+                "name": "source",
+                "type": "address"
+            }
+        ],
+        "name": "AssetSourceUpdated",
+        "type": "event"
+    },
+    {
+        "anonymous": False,
+        "inputs": [
+            {
+                "indexed": True,
+                "internalType": "address",
+                "name": "baseCurrency",
+                "type": "address"
+            },
+            {
+                "indexed": False,
+                "internalType": "uint256",
+                "name": "baseCurrencyUnit",
+                "type": "uint256"
+            }
+        ],
+        "name": "BaseCurrencySet",
+        "type": "event"
+    },
+    {
+        "anonymous": False,
+        "inputs": [
+            {
+                "indexed": True,
+                "internalType": "address",
+                "name": "fallbackOracle",
+                "type": "address"
+            }
+        ],
+        "name": "FallbackOracleUpdated",
+        "type": "event"
+    },
+    {
+        "inputs": [],
+        "name": "ADDRESSES_PROVIDER",
+        "outputs": [
+            {
+                "internalType": "contract IPoolAddressesProvider",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "BASE_CURRENCY",
+        "outputs": [
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "BASE_CURRENCY_UNIT",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "asset",
+                "type": "address"
+            }
+        ],
+        "name": "getAssetPrice",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address[]",
+                "name": "assets",
+                "type": "address[]"
+            }
+        ],
+        "name": "getAssetsPrices",
+        "outputs": [
+            {
+                "internalType": "uint256[]",
+                "name": "",
+                "type": "uint256[]"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "getFallbackOracle",
+        "outputs": [
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "asset",
+                "type": "address"
+            }
+        ],
+        "name": "getSourceOfAsset",
+        "outputs": [
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address[]",
+                "name": "assets",
+                "type": "address[]"
+            },
+            {
+                "internalType": "address[]",
+                "name": "sources",
+                "type": "address[]"
+            }
+        ],
+        "name": "setAssetSources",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "fallbackOracle",
+                "type": "address"
+            }
+        ],
+        "name": "setFallbackOracle",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    }
+]
--- scripts/call_contract_functions.py
+++ scripts/call_contract_functions.py
@@ -2,20 +2,29 @@
 
 
 from web3 import Web3
-from scripts.aave_pool_abi import ABI as AAVE_ABI
+from aave_pool_abi import ABI as AAVE_POOL_ABI
+from aave_oracle_abi import ABI as AAVE_ORACLE_ABI
 from erc20_abi import ABI as ERC20_ABI
+
 
 # Fill in your infura API key here
 web3 = Web3(Web3.HTTPProvider("https://goerli.infura.io/v3/837e5a22f48a42a1be56cafa6aaacc5f"))
 
-address = "0x368EedF3f56ad10b9bC57eed4Dac65B26Bb667f6"
-#address = "0x480B8b39d1465b8049fbf03b8E0a072Ab7C9A422"
+#address = "0x5bed0810073cc9f0DacF73C648202249E87eF6cB" # Goerli Aave Oracle Address
+#address = "0x368EedF3f56ad10b9bC57eed4Dac65B26Bb667f6" # Goerli Aave Pool Address
+address = "0xA2025B15a1757311bfD68cb14eaeFCc237AF5b43" # Goerli USDC
 
-contract = web3.eth.contract(address=address, abi=AAVE_ABI)
+contract = web3.eth.contract(address=address, abi=ERC20_ABI)
 
-user_account_data = contract.functions.getUserAccountData("0x3f9E4A6120aB7868485602241AbE9D85d6F9E382").call()
-#user_account_data = contract.functions.balanceOf("0x3f9E4A6120aB7868485602241AbE9D85d6F9E382").call()
-# user_account_data = contract.functions.getReservesList().call()
+#result = contract.functions.getUserAccountData("0x3f9E4A6120aB7868485602241AbE9D85d6F9E382").call()
+#result = contract.functions.getReserveData("0xA2025B15a1757311bfD68cb14eaeFCc237AF5b43").call() #wBTC
+#result = contract.functions.getConfiguration("0xA2025B15a1757311bfD68cb14eaeFCc237AF5b43").call()  #USDC
+#result = contract.functions.getConfiguration("0x2e3A2fb8473316A02b8A297B982498E661E1f6f5").call()  #wETH
+#result = contract.functions.balanceOf("0x3f9E4A6120aB7868485602241AbE9D85d6F9E382").call()
+#result = contract.functions.getReservesList().call()
 
-print(user_account_data)
+#result = contract.functions.balanceOf("0x71059a9f54B00e1933dc6e02239a1471f58518dD").call()
+result = contract.functions.allowance("0x71059a9f54B00e1933dc6e02239a1471f58518dD", "0x270D6F7620C30F300af545798061a251fae09319").call()
+
+print(result)
 
